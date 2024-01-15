45e156535c77eb82c43925147cef816150d9cf5a
haelim12 <sunny9612@naver.com>
2024-01-15 23:17:22 +0900

    페이지 SEO 작업

--- src/pages/Webd.jsx
+++ src/pages/Webd.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Main from "../components/section/Main";
 
-const Wedd = () => {
-  return <Main>Wedd</Main>;
+const Webd = () => {
+  return (
+    <Main
+      title="웹디자인 기능사"
+      description="웹디자인 기능사 튜토리얼 강의입니다."
+    >
+      Webd
+    </Main>
+  );
 };
 
-export default Wedd;
+export default Webd;
